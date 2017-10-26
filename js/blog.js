bbe28c11525311681fe36ad02bf029480baf39d8
huangdengdeng <744226174@qq.com>
2017-10-26 13:09:07 +0800

    提交信息

--- js/blog.js
+++ js/blog.js
@@ -16,9 +16,12 @@ function JsonCreateheader() {
 			if(test.readyState == 4 && test.status == 200) {
 				var obj = JSON.parse(test.responseText);
 				createHeader(obj);
+				createSection(obj);
+				createFooter(obj);
 			}
 		};
 	}
+
 	function createLi(obj) {
 		var myheader = document.getElementById("myheader");
 		var oUl = document.createElement("ul");
@@ -48,12 +51,12 @@ function JsonCreateheader() {
 		console.log(obj[0].name);
 		var oDiv = document.getElementsByClassName("blog_header_intrduce");
 		var oPTit = document.createElement("p");
-		var oTit= document.createTextNode(obj[0].user_int[0]);
+		var oTit = document.createTextNode(obj[0].user_int[0]);
 		oPTit.appendChild(oTit);
-		oPTit.className="blog_tit_intrduce";
+		oPTit.className = "blog_tit_intrduce";
 		oDiv[0].appendChild(oPTit);
-		for(var i = 0, count = obj[0].user_int.length-1; i < count; i++) {
-			var text = obj[0].user_int[i+1];
+		for(var i = 0, count = obj[0].user_int.length - 1; i < count; i++) {
+			var text = obj[0].user_int[i + 1];
 			var oP = document.createElement("p");
 			var oTest = document.createTextNode(text);
 			oP.className = "blog_text_intrduce";
@@ -62,6 +65,148 @@ function JsonCreateheader() {
 			console.log(count);
 		}
 
+	}
+
+	function createSection(obj) {
+//		动态创建section
+		var oBigDiv = document.getElementsByClassName("blog_introduce");
+		var oDiv = document.createElement("div");
+		//		动态创建两个同样样式div
+		for(var i = 0, count = obj[0].userlive.length; i < count; i++) {
+			console.log("rt8ug");
+			var oDivBox = oBigDiv[0].appendChild(oDiv);
+			var oP = document.createElement("p");
+			var oPS = document.createElement("p");
+			var oImg = document.createElement("img");
+			var ImgSrc = obj[0].userlive[i].imgurl;
+			var oDivBoxText = document.createTextNode(obj[0].userlive[i].text);
+			//			console.log(ImgSrc);
+			oPS.appendChild(oDivBoxText);
+			oImg.setAttribute("src", " ");
+			oImg.attributes[0].value = ImgSrc;
+			var oDivSmall = document.createElement("div");
+			oDivSmall.className = "blog_mylive_details";
+			oDivSmall.appendChild(oImg);
+			oDivSmall.appendChild(oPS);
+			oDivBox.className = "blog_introduce_mylive";
+			oP.className = "blog_tit_font";
+			oP.innerHTML = obj[0].userlive[i].tit;
+			oDivBox.appendChild(oP);
+			oDivBox.appendChild(oDivSmall);
+			oBigDiv[0].appendChild(oDivBox);
+		}
+
+		//动态创建联系方式盒子
+		//		盒子左边部分
+		var oBoxOther = document.getElementsByClassName("blog_other_details");
+		var oListDiv = document.createElement("div");
+		var oListP = document.createElement("p");
+		var oFrag = document.createDocumentFragment();
+		var oListUl = document.createElement("ul");
+		for(var i = 0, count = obj[0].other.list_left.list.length; i < count; i++) {
+			var oListLi = document.createElement("li");
+			var oListA = document.createElement("a");
+			var txt = obj[0].other.list_left.list[i];
+			console.log(txt);
+			var listtext = document.createTextNode(obj[0].other.list_left.list[i]);
+			//			console.log(listtext);
+			oListA.appendChild(listtext);
+			oListLi.appendChild(oListA);
+			oFrag.appendChild(oListLi);
+		}
+		oListUl.appendChild(oFrag);
+		oListDiv.className = "blog_other_links";
+		oListP.className = "blog_tit_font";
+		oListP.innerHTML = obj[0].other.list_left.tit;
+		oListDiv.appendChild(oListP);
+		var oListPT = document.createElement("p");
+		oListPT.innerHTML = obj[0].other.list_left.intr;
+		oListDiv.appendChild(oListPT);
+		oListDiv.appendChild(oListUl);
+		oBigDiv[0].appendChild(oBoxOther[0]).appendChild(oListDiv);
+
+		//动态创建盒子右边部分
+		var oConDiv = document.createElement("div");
+		var oConP = document.createElement("p");
+		var oConTit = document.createTextNode(obj[0].other.list_right.tit);
+		console.log(oConTit);
+		
+		oConDiv.className = "blog_other_email";
+		oConP.className = "blog_tit_font";
+		oConP.appendChild(oConTit);
+		oConDiv.appendChild(oConP);
+		for(var i = 0, count = obj[0].other.list_right.list.length; i < count; i++) {
+			var oConPList = document.createElement("p");
+			var oConList= document.createTextNode(obj[0].other.list_right.list[i]);
+			oConPList.appendChild(oConList);
+			oConDiv.appendChild(oConPList);
+		}
+		oBoxOther[0].appendChild(oConDiv);
+//		创建大盒子右边的分享
+		var oPicBox=document.getElementsByClassName("blog_share");
+		var oBoxList=document.getElementsByClassName("blog_share_pic");
+		var oPicTit = document.createElement("p");
+		var oPicTitTxt=document.createTextNode(obj[0].other_pic.tit);
+		var oBoxPicList=document.createElement("div");
+		oPicTit.className="blog_tit_font";
+		oBoxPicList.className="blog_share_piclist";
+		oPicTit.appendChild(oPicTitTxt);
+		oBoxList[0].appendChild(oPicTit);
+		for(var i=0,count=obj[0].other_pic.ImgURL.length;i<count;i++){
+			var oBoxPicBox=document.createElement("div");
+			var oPicA=document.createElement("a");
+			var oBoxImg=document.createElement("img");
+			var BoxImgSrc=obj[0].other_pic.ImgURL[i];
+			console.log(BoxImgSrc);
+			oBoxPicBox.className="blog__piclist_layout";
+			oBoxImg.setAttribute("src"," ");
+			oBoxImg.attributes[0].value = BoxImgSrc;
+			oPicA.appendChild(oBoxImg);
+			oBoxPicBox.appendChild(oPicA);
+			oBoxPicList.appendChild(oBoxPicBox);
+		}
+		oBoxList[0].appendChild(oBoxPicList);
+	}
+	function createFooter(obj){
+		var oDiv=document.getElementsByClassName("blog_skill_details");
+		var oTit=document.getElementsByClassName("blog_footer_tit_font");
+		var oTwoTitp=document.createTextNode(obj[0].footer.footerTwo.tit);
+		var oThreeTit=document.createTextNode(obj[0].footer.footerThree.tit);
+		oTit[0].appendChild(document.createTextNode(obj[0].footer.footerOne.tit));
+		oTit[1].appendChild(oTwoTitp);
+		oTit[2].appendChild(oThreeTit);
+		console.log(obj[0].footer.footerOne.tit);
+		for(var i=0,count=obj[0].footer.footerOne.txt.length;i<count;i++){
+			var oPt=document.createElement("p");
+			var oPTxt=document.createTextNode(obj[0].footer.footerOne.txt[i]);
+			oPt.className="blog_footer_text_font";
+			oPt.appendChild(oPTxt);
+			oDiv[0].appendChild(oPt);
+		}
+//		创建中间div的MOM
+		var oTwoDiv=document.getElementsByClassName("blog_other_details_")[0];
+		var oPtwo=document.createElement("p");
+		var oImg=document.createElement("img");
+		var oImgURL=obj[0].footer.footerTwo.imgURL;
+		var oPtwoTxt=document.createTextNode(obj[0].footer.footerTwo.txt[0]);
+		oPtwo.className="blog_footer_text_font";
+		oPtwo.appendChild(oPtwoTxt);
+		oImg.setAttribute("src","");
+		oImg.attributes[0].value=oImgURL;
+		oTwoDiv.appendChild(oImg);
+		oTwoDiv.appendChild(oPtwo);
+		
+		for(var i=0,count=obj[0].footer.footerThree.txt.length;i<count;i++){
+			var oDivTr=document.getElementsByClassName("blog_more_details")[0];
+			var oP=document.createElement("p");
+			var oPTxt=document.createTextNode(obj[0].footer.footerThree.txt[i]);
+			oP.className="blog_footer_text_font";
+			oP.appendChild(oPTxt);
+			oDivTr.className="blog_more_details";
+			oDivTr.appendChild(oP);
+		}
+		
+		
 	}
 }
 window.onload = function() {
